4bc6cf27ff32df033558a37cacbdc48ce7d84541
Batuhan San <54415339+batusan@users.noreply.github.com>
2021-04-07 18:13:15 +0300

    Deleting misconfigured parts

--- services/DAL_service.js
+++ services/DAL_service.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sociality'
+    user: 'YOUR_DATABASE_USERNAME',
+    password: 'YOUR_DATABASE_PASSWORD',
+    database: 'YOUR_DATABASE_NAME',
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log('MySQL succesfully connected.');
+    //console.log('MySQL succesfully connected.');
 });
 
 module.exports = connection;
