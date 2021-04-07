e53db0a7289e5989e06df2ecd8446595719bdb9a
batusan <batuhan.san1@gazi.edu.tr>
2021-04-07 23:17:08 +0300

    Docker integration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-var cors = require('cors');
+const path = require('path');
+const cors = require('cors');
+
 
 const products = require('./routes/products');
 
-const port = 3000;
+const port = process.env.PORT || 8080;
 var app = express();
 
 
@@ -12,11 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', express.static(path.join(__dirname, 'frontend')))
 
-
-app.get('/', (req, res) => {
-    res.json({ message: 'welcome to web service , try to /api/ !' });
-});
 
 app.use('/api/', products);
 
--- services/DAL_service.js
+++ services/DAL_service.js
@@ -1,23 +1,45 @@
 const mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'YOUR_DATABASE_USERNAME',
-    password: 'YOUR_DATABASE_PASSWORD',
-    database: 'YOUR_DATABASE_NAME',
+    host: 'mysql',
+    user: 'dbconn',
+    password: 'password',
+    database: 'etsy',
 });
+
 /*
  *Database schema for product
     CREATE TABLE IF NOT EXISTS products (
-	    product_id INT AUTO_INCREMENT PRIMARY KEY,
-	    product_name VARCHAR(500),
-	    product_image VARCHAR(500),
-	    product_price VARCHAR(50)
+        product_id INT AUTO_INCREMENT,
+        product_name VARCHAR(500),
+        product_image VARCHAR(500),
+        product_price VARCHAR(50),
+        PRIMARY KEY(product_id)
     ); 
 */
-connection.connect(function (err) {
-    if (err) throw err;
-    //console.log('MySQL succesfully connected.');
-});
+
+
+
+function handleDisconnect() {                                                  // the old one cannot be reused.
+
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
 
 module.exports = connection;
