fa2066ce373af8e4c88e6c972d6a306db0babc61
Batuhan San <54415339+batusan@users.noreply.github.com>
2021-04-07 18:14:13 +0300

    Update DAL_service.js

--- services/DAL_service.js
+++ services/DAL_service.js
@@ -6,7 +6,15 @@ let connection = mysql.createConnection({
     password: 'YOUR_DATABASE_PASSWORD',
     database: 'YOUR_DATABASE_NAME',
 });
-
+/*
+ *Database schema for product
+    CREATE TABLE IF NOT EXISTS products (
+	    product_id INT AUTO_INCREMENT PRIMARY KEY,
+	    product_name VARCHAR(500),
+	    product_image VARCHAR(500),
+	    product_price VARCHAR(50)
+    ); 
+*/
 connection.connect(function (err) {
     if (err) throw err;
     //console.log('MySQL succesfully connected.');
